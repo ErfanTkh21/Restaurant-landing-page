b503a39ea12c20593406fd79373f725e9dbc5697
Erfan <erfantkh20073@gmail.com>
2025-05-20 15:28:05 +0330

    order buttons feature

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,38 @@
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const quantityEl = document.getElementById("quantity");
+const totalEl = document.getElementById("total");
 
 let quantity = parseInt(quantityEl.textContent);
+const pricePerItem = 5.39;
+
+function updateDisplay() {
+  // آپدیت مقدار سفارش
+  quantityEl.textContent = quantity;
+
+  // انیمیشن عدد سفارش
+  quantityEl.classList.remove("pop-animation");
+  void quantityEl.offsetWidth;
+  quantityEl.classList.add("pop-animation");
+
+  // آپدیت قیمت کل
+  totalEl.textContent = (quantity * pricePerItem).toFixed(2);
+}
 
 increaseBtn.addEventListener("click", () => {
   quantity++;
-  quantityEl.textContent = quantity;
+  updateDisplay();
 });
 
 decreaseBtn.addEventListener("click", () => {
   if (quantity > 0) {
     quantity--;
-    quantityEl.textContent = quantity;
+    updateDisplay();
   }
+});
+
+updateDisplay(); 
+
+document.getElementById("order-btn").addEventListener("click", function () {
+  window.location.href = "order.html";
 });
